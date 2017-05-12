be5376d3440d36e4483f6d440cc089e800670353
Tangoyzx <tangoyzx@gmail.com>
2017-05-12 17:47:09 +0800

    init water

--- Assets/Scripts/MapGen/MapGen.cs
+++ Assets/Scripts/MapGen/MapGen.cs
@@ -4,15 +4,76 @@ using System.Collections.Generic;
 using csDelaunay;
 
 public class MapGen {
-	public static int SIZE = 600;
+	private int size;
+	private Texture2D perlinTex;
 	private List<Vector2> points;
-	private List<VEdge> edges;
-	private List<VCorner> corners;
-	private List<VCenter> centers;
+	private List<VEdge> edges = new List<VEdge>();
+	private List<VCorner> corners = new List<VCorner>();
+	private List<VCenter> centers = new List<VCenter>();
+	private Dictionary<int, List<VCorner>> _cornerMap = new Dictionary<int, List<VCorner>>();
+	public MapGen(int size, Texture2D perlinTex)
+	{
+		this.size = size;
+		this.perlinTex = perlinTex;
+	}
+	public void Create(int n) {
+		var points = GetRandomPoint(n);
+		// List<Vector2> points = new List<Vector2>();
+        // points.Add(new Vector2(100, 100));
+        // points.Add(new Vector2(100, 300));
+		var voronoi = new Voronoi(points,new Rect(0, 0, this.size, this.size), 3);
 
-	private Dictionary<int, List<VCorner>> _cornerMap;
-	public void Create(List<Vector2> points, Voronoi voronoi) {
+		points.Clear();
+		foreach (KeyValuePair<Vector2,Site> kv in voronoi.SitesIndexedByLocation) {
+			points.Add(kv.Key);
+		}
 
+		BuildGraph(points, voronoi);
+		// voronoi.Dispose();
+		AssignCornerElevations();
+
+
+		drawDebugEdge(points, voronoi);
+	}
+
+	private void drawDebugEdge(List<Vector2> points, Voronoi voronoi) {
+		Debug.DrawLine(new Vector3(), new Vector3(this.size, 0, 0), Color.blue, float.MaxValue);
+		Debug.DrawLine(new Vector3(this.size, 0, 0), new Vector3(this.size, this.size, 0), Color.blue, float.MaxValue);
+		Debug.DrawLine(new Vector3(this.size, this.size, 0), new Vector3(0, this.size, 0), Color.blue, float.MaxValue);
+		Debug.DrawLine(new Vector3(0, this.size, 0), new Vector3(0, 0, 0), Color.blue, float.MaxValue);
+
+		foreach(var q in corners)
+		{
+			var point = q.point;
+			
+			var color = (q.water)?Color.blue:(q.elevation < 100)?Color.red:Color.green;
+			Debug.DrawLine(new Vector3(point.x - 2, point.y - 2, 0), new Vector3(point.x + 2, point.y - 2, 0), color,float.MaxValue);
+			Debug.DrawLine(new Vector3(point.x + 2, point.y - 2, 0), new Vector3(point.x + 2, point.y + 2, 0), color,float.MaxValue);
+			Debug.DrawLine(new Vector3(point.x + 2, point.y + 2, 0), new Vector3(point.x - 2, point.y + 2, 0), color,float.MaxValue);
+			Debug.DrawLine(new Vector3(point.x - 2, point.y + 2, 0), new Vector3(point.x - 2, point.y - 2, 0), color,float.MaxValue);
+		}
+
+		foreach(var edge in edges)
+		{
+			if (edge.v0 != null && edge.v1 != null) {
+				var color = (edge.v0.water || edge.v1.water)?Color.blue:Color.green;
+				Debug.DrawLine(new Vector3(edge.v0.point.x, edge.v0.point.y, 0), new Vector3(edge.v1.point.x, edge.v1.point.y, 0), color, float.MaxValue);
+				Debug.Log(edge.v0.point + "|" + edge.v1.point);
+			}
+
+			if (edge.d0 != null && edge.d1 != null) {
+				// Debug.DrawLine(new Vector3(edge.d0.point.x, edge.d0.point.y, 0), new Vector3(edge.d1.point.x, edge.d1.point.y, 0), Color.red, float.MaxValue);
+			}
+		}
+	}
+
+	private List<Vector2> GetRandomPoint(int n) {
+		var list = new List<Vector2>();
+		for(int i = 0; i < n; i++)
+		{
+			list.Add(new Vector2(Random.Range(0, this.size), Random.Range(0, this.size)));
+		}
+		return list;
 	}
 
 	private void BuildGraph(List<Vector2> points, Voronoi voronoi)
@@ -53,8 +114,10 @@ public class MapGen {
 
 			edge.v0 = MakeCorner(vedge.p0);
 			edge.v1 = MakeCorner(vedge.p1);
-			edge.d0 = centerLookup[dedge.p0];
-			edge.d1 = centerLookup[dedge.p1];
+			if (centerLookup.ContainsKey(dedge.p0))
+				edge.d0 = centerLookup[dedge.p0];
+			if (centerLookup.ContainsKey(dedge.p1))
+				edge.d1 = centerLookup[dedge.p1];
 
 			if (edge.d0 != null) edge.d0.borders.Add(edge);
 			if (edge.d1 != null) edge.d1.borders.Add(edge);
@@ -91,37 +154,6 @@ public class MapGen {
 		}
 	}
 
-	private void ImproveCorners() {
-		var newCorners = new List<Vector2>(corners.Count);
-		foreach (var q in corners)
-		{
-			if (q.border) {
-				newCorners[q.index] = q.point;
-			} else {
-				var point = new Vector2();
-				foreach(var r in q.touches)
-				{
-					point.x += r.point.x;
-					point.y += r.point.y;
-				}
-				point.x /= q.touches.Count;
-				point.y /= q.touches.Count;
-			}
-		}
-
-		for(int i = 0; i < corners.Count; i++)
-		{
-			corners[i].point = newCorners[i];
-		}
-
-		foreach(var edge in edges)
-		{
-			if ((edge.v0 != null) && (edge.v1 != null)) {
-				edge.midpoint = Vector2.Lerp(edge.v0.point, edge.v1.point, 0.5f);
-			}
-		}
-	}
-
 	private void AddToCornerList(List<VCorner> v, VCorner x) {
 		if (x != null && v.IndexOf(x) < 0) v.Add(x);
 	}
@@ -136,6 +168,7 @@ public class MapGen {
 		int bucket;
 		for(bucket = (int)(point.x) - 1; bucket <= (int)(point.x) + 1; bucket++)
 		{
+			if (!_cornerMap.ContainsKey(bucket)) continue;
 			foreach(var qq in _cornerMap[bucket])
 			{
 				var dx = point.x - qq.point.x;
@@ -151,11 +184,52 @@ public class MapGen {
 		q.index = corners.Count;
 		corners.Add(q);
 		q.point = point;
-		q.border = (point.x == 0 || point.x == SIZE || point.y == 0 || point.y == SIZE);
+		q.border = (point.x == 0 || point.x == size || point.y == 0 || point.y == size);
 		q.touches = new List<VCenter>();
 		q.protrudes = new List<VEdge>();
 		q.adjacent = new List<VCorner>();
 		_cornerMap[bucket].Add(q);
 		return q;
+	}
+
+	private void AssignCornerElevations() {
+		var queue = new Queue<VCorner>();
+		foreach(var q0 in corners) {
+			q0.water = !IsInLand(q0.point);
+		}
+
+		foreach(var q1 in corners)
+		{
+			if (q1.border) {
+				q1.elevation = 0.0f;
+				queue.Enqueue(q1);
+			}
+			else {
+				q1.elevation = float.PositiveInfinity;
+			}
+
+			while (queue.Count > 0) {
+				var q = queue.Dequeue();
+
+				foreach(var s in q.adjacent) {
+					var newElevation = 0.01f + q.elevation;
+					if (!q.water && !s.water) {
+						newElevation += 1;
+					}
+
+					if (newElevation < s.elevation) {
+						s.elevation = newElevation;
+						queue.Enqueue(s);
+					}
+				}
+			}
+		}
+	}
+
+	private bool IsInLand(Vector2 point)
+	{
+		var q = new Vector2(point.x / size * 2.0f - 1.0f, point.y / size * 2.0f - 1.0f);
+		var c = perlinTex.GetPixel((int)(q.x+1) * perlinTex.width / 2, (int)(q.y+1) * perlinTex.height / 2);
+		return c.r > (0.3 + 0.3 * q.sqrMagnitude);
 	}
 }
---
+++ Assets/Scripts/Test1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+	public Texture2D perlinTex;
+	// Use this for initialization
+	void Start () {
+		var mapgen = new MapGen(512, perlinTex);
+		mapgen.Create(200);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
